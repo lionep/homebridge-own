1691a6d183e220c666dfe938919c2cd318715768
Lionel Penaud <penaud.lionel@gmail.com>
2018-07-05 20:38:14 +0200

    fix(own)

--- src/test.js
+++ src/test.js
@@ -1,29 +1,35 @@
 import OwnSocketUtils from './utils/own-socket';
 
+const ip = '192.168.1.250';
+// const ip = '10.0.0.178';
+
 // Status
 const start = Date.now();
-OwnSocketUtils.send('10.0.0.23', 20000, '*#1*11##', (err, res) => {
+OwnSocketUtils.send(ip, 20000, true, '*#1*0##', (err, res) => {
   if (err) console.error(err);
   console.log(res);
   console.log(`${Date.now() - start}ms`);
 });
-OwnSocketUtils.send('10.0.0.23', 20000, '*#1*12##', (err, res) => {
+
+/*
+OwnSocketUtils.send(ip, 20000, '*#1*12##', (err, res) => {
   if (err) console.error(err);
   console.log(res);
   console.log(`${Date.now() - start}ms`);
 });
-OwnSocketUtils.send('10.0.0.23', 20000, '*#1*15##', (err, res) => {
+OwnSocketUtils.send(ip, 20000, '*#1*15##', (err, res) => {
   if (err) console.error(err);
   console.log(res);
   console.log(`${Date.now() - start}ms`);
 });
-OwnSocketUtils.send('10.0.0.23', 20000, '*#1*16##', (err, res) => {
+OwnSocketUtils.send(ip, 20000, '*#1*16##', (err, res) => {
   if (err) console.error(err);
   console.log(res);
   console.log(`${Date.now() - start}ms`);
 });
-OwnSocketUtils.send('10.0.0.23', 20000, '*#1*17##', (err, res) => {
+OwnSocketUtils.send(ip, 20000, '*#1*17##', (err, res) => {
   if (err) console.error(err);
   console.log(res);
   console.log(`${Date.now() - start}ms`);
 });
+*/
--- src/utils/own-socket.js
+++ src/utils/own-socket.js
@@ -1,19 +1,22 @@
 import {chain, isArray} from 'lodash';
 import net from 'net';
+import Promise from 'bluebird';
 
 export default class OwnSocketUtils {
   static ACK = '*#*1##';
   static NACK = '*#*0##';
+  static CONN = '*99*9##';
 
   static MSG_SEP = '##';
 
-  constructor(host, port) {
+  constructor(host, port, useConn) {
     this.host = host;
     this.port = port;
+    this.useConn = useConn;
   }
 
   send(message, callback) {
-    OwnSocketUtils.send(this.host, this.port, message, callback);
+    OwnSocketUtils.send(this.host, this.port, this.useConn, message, callback);
   }
 
   static readResponse(res) {
@@ -25,37 +28,84 @@ export default class OwnSocketUtils {
     return {msg: res};
   }
 
-  static send(host, port, message, callback) {
+  static send(host, port, useConn, message, callback) {
+    console.log(`Establishing connection to ${host}`);
     const socket = new net.Socket();
     let responses = [];
+
+    let connAck = false;
+    let connSend = false;
 
     // Send message after connection
     socket.connect({
       port,
       host
     }, () => {
-      if (isArray(message)) {
-        message.forEach((messageItem) => {
-          socket.write(messageItem);
-        });
-        socket.end();
-      } else {
-        socket.end(message);
-      }
+      return Promise.resolve()
+        .then(() => {
+          if (!useConn) return true;
+          return new Promise((resolve, reject) => {
+            socket.once('data', (buf) => {
+              if (buf.toString() === OwnSocketUtils.ACK) return resolve();
+              return reject();
+            });
+          })
+          .tap(() => {
+            connAck = true;
+            console.log('Connected.');
+          })
+          .then(() => {
+            console.log('Send CONN');
+            socket.write(OwnSocketUtils.CONN);
+            return new Promise((resolve, reject) => {
+              socket.once('data', (buf) => {
+                if (buf.toString() === OwnSocketUtils.ACK) {
+                  // connSend = true;
+                  return resolve();
+                }
+                return reject();
+              });
+            });
+          })
+          .tap(() => {
+            console.log('Authorized.');
+          })
+        })
+        .then(() => {
+          if (isArray(message)) {
+            message.forEach((messageItem) => {
+              console.log(`write ${messageItem}`);
+              socket.write(messageItem);
+            });
+            // socket.end();
+          } else {
+            console.log(`write single ${message}`);
+            socket.write(message);
+            // socket.end();
+          }
+        })
+
+
     });
 
     // Send response on end
     socket.on('end', () => {
+      console.log('end');
       let hasError = false;
       const returnedResponses = [];
       responses.forEach((response, index) => {
         const res = OwnSocketUtils.readResponse(response);
-        if (index === 0 && res.ack) return; // Ignore first ack
+        // if (index === 0 && res.ack) {
+        //   console.log('ACK');
+        //   return; // Ignore first ack
+        // }
         if (res.ack === false) {
+          console.log('NACK');
           hasError = true;
           return;
         }
         if (res.msg) {
+          console.log(`RES : ${res.msg}`);
           returnedResponses.push(res.msg);
         }
       });
@@ -65,6 +115,18 @@ export default class OwnSocketUtils {
 
     // Append data to responses (resplit if necessary)
     socket.on('data', (buf) => {
+      const res = buf.toString();
+      if (res === OwnSocketUtils.ACK && !connAck) {
+        connAck = true;
+        return;
+      }
+      if (res === OwnSocketUtils.ACK && !connSend) {
+        connSend = true;
+        return;
+      }
+      if (res === OwnSocketUtils.ACK && connSend && connAck) {
+        socket.end();
+      }
       responses = responses.concat(chain(buf.toString().split(OwnSocketUtils.MSG_SEP))
         .filter((res) => res !== '')
         .map((res) =>
